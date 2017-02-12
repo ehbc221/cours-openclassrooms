16335aa24e7c91c5d22762b1ad023e802a63b309
ehbc221 <ehbc221@gmail.com>
2017-02-12 16:24:15 +0000

    V2.6 - Updated indentations to Tabs

--- src/OC/CoreBundle/Controller/CoreController.php
+++ src/OC/CoreBundle/Controller/CoreController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //src/OC/CoreBundle/Controller/CoreController.php
 
 namespace OC\CoreBundle\Controller;
@@ -9,19 +9,19 @@ use Symfony\Component\HttpFoundation\Request;
 class CoreController extends Controller
 {
 
-  public function indexAction()
-  {
-    return $this->render('OCCoreBundle:Core:index.html.twig');
-  }
+	public function indexAction()
+	{
+		return $this->render('OCCoreBundle:Core:index.html.twig');
+	}
 
-  public function contactAction(Request $request)
-  {
-    $session = $request
-      ->getSession()
-      ->getFlashBag()
-      ->add('info', 'La page de contact n\'est pas encore disponible, merci de revenir plus tard.');
+	public function contactAction(Request $request)
+	{
+		$session = $request
+			->getSession()
+			->getFlashBag()
+			->add('info', 'La page de contact n\'est pas encore disponible, merci de revenir plus tard.');
 
-    return $this->redirectToRoute('oc_core_home');
-  }
-  
+		return $this->redirectToRoute('oc_core_home');
+	}
+
 }
--- src/OC/PlatformBundle/Antispam/OCAntispam.php
+++ src/OC/PlatformBundle/Antispam/OCAntispam.php
@@ -5,25 +5,25 @@ namespace OC\PlatformBundle\Antispam;
 
 class OCAntispam
 {
-  private $mailer;
-  private $locale;
-  private $minLength;
+	private $mailer;
+	private $locale;
+	private $minLength;
 
-  public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
-  {
-    $this->mailer    = $mailer;
-    $this->locale    = $locale;
-    $this->minLength = (int) $minLength;
-  }
+	public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
+	{
+		$this->mailer    = $mailer;
+		$this->locale    = $locale;
+		$this->minLength = (int) $minLength;
+	}
 
-  /**
-   * Vérifie si le texte est un spam ou non
-   *
-   * @param string $text
-   * @return bool
-   */
-  public function isSpam($text)
-  {
-    return strlen($text) < $this->minLength;
-  }
+	/**
+	 * Vérifie si le texte est un spam ou non
+	 *
+	 * @param string $text
+	 * @return bool
+	 */
+	public function isSpam($text)
+	{
+		return strlen($text) < $this->minLength;
+	}
 }
